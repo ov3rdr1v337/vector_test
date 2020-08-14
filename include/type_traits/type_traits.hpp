c3e9864516c76afefd789a271f0ed853217b3d4b
kvoistinov <kvoistinov@gmail.com>
2020-08-14 22:26:44 +0300

    -Added new hpp file for grouping all current versions of type traits

---
+++ include/type_traits/type_traits.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "helper_traits.hpp"
+#include "compiler_sfinae_intrinsics.hpp"
+#include "type_modifications.hpp"
+#include "primary_type_traits.hpp"
+#include "type_properties.hpp"
