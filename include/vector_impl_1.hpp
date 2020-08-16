2ff2451aac1253b32800e0036a99efe482f2ca7d
ov3rdr1v337 <ivan.kamynin1994@gmail.com>
2020-08-16 23:01:28 +0300

    -Added vector push implementations for universe references

--- include/vector_impl_1.hpp
+++ include/vector_impl_1.hpp
@@ -92,10 +92,10 @@ namespace container
             >
         traits::enable_if
             <
-                traits::is_trivially_copyable<u_arg>::value, 
+                traits::is_trivially_copyable<std::decay_t<u_arg>>::value, 
                 void
             > ::type
-        push_back( const t_arg& element )
+        push_back( u_arg&& element )
         {
             if ( m_end == m_cap_end )
             {
@@ -124,69 +124,75 @@ namespace container
             std::memcpy( m_end++, &element, sizeof( t_arg ) );
         }
 
-        // template
-        //     <
-        //         class u_arg = t_arg
-        //     >
-        // std::enable_if_t
-        //     <
-        //        !std::is_trivially_copyable<t_arg>        ::value &&
-        //         std::is_nothrow_move_constructible<t_arg>::value, 
-        //         void
-        //     >
-        // push_back( const t_arg& element )
-        // {
-        //     if ( m_end == m_cap_end )
-        //     {
-        //         const size_t old_size = reinterpret_cast<char*>( m_end ) - 
-        //                                 reinterpret_cast<char*>( m_begin );
+        template
+            <
+                class u_arg = t_arg
+            >
+        traits::enable_if
+            <
+               !std::is_trivially_copyable<std::decay_t<u_arg>>        ::value &&
+                std::is_nothrow_move_constructible<std::decay_t<u_arg>>::value, 
+                void
+            > ::type
+        push_back( u_arg&& element )
+        {
+            if ( m_end == m_cap_end )
+            {
+                const size_t old_size = reinterpret_cast<char*>( m_end ) - 
+                                        reinterpret_cast<char*>( m_begin );
 
-        //         const size_t new_size = old_size << grow_constant;
+                const size_t new_size = old_size << grow_constant;
 
-        //         char *m_begin_new     = reinterpret_cast<char*> ( operator new ( new_size ) );
+                t_arg *m_begin_new    = reinterpret_cast<t_arg*>( operator new ( new_size ) );
 
-        //         m_end                 = reinterpret_cast<t_arg*>( m_begin_new + old_size );
-        //         m_cap_end             = reinterpret_cast<t_arg*>( m_begin_new + new_size );
+                m_end                 = reinterpret_cast<t_arg*>( m_begin_new + old_size );
+                m_cap_end             = reinterpret_cast<t_arg*>( m_begin_new + new_size );
 
-        //         for ( ; m_begin_new != m_end; ++m_begin, ++m_begin_new )
-        //             new ( m_begin_new ) t_arg( std::move( *m_begin ) );
-        //     }
+                for ( t_arg *m_begin_new_it = m_begin_new; m_begin_new_it != m_end; ++m_begin, ++m_begin_new_it )
+                    new ( m_begin_new_it ) t_arg( std::move( *m_begin ) );
 
-        //     new ( m_begin_new ) t_arg( element );
-        // }
+                m_begin               = reinterpret_cast<t_arg*>( m_begin_new );                    
+            }
 
-        // template
-        //     <
-        //         class u_arg = t_arg
-        //     >
-        // std::enable_if_t
-        //     <
-        //        !std::is_trivially_copyable<t_arg>        ::value &&
-        //        !std::is_nothrow_move_constructible<t_arg>::value &&
-        //        !std::is_copy_constructible<t_arg>        ::value ), 
-        //         void
-        //     >
-        // push_back( const t_arg& element )
-        // {
+            new ( m_end++ ) t_arg( element );
+        }
 
-        // }
+        template
+            <
+                class u_arg = t_arg
+            >
+        traits::enable_if
+            <
+               !std::is_trivially_copyable<std::decay_t<u_arg>>        ::value &&
+               !std::is_nothrow_move_constructible<std::decay_t<u_arg>>::value &&
+                std::is_copy_constructible<std::decay_t<u_arg>>        ::value , 
+                void
+            > ::type
+        push_back( u_arg&& element )
+        {
+            if ( m_end == m_cap_end )
+            {
+                const size_t old_size = reinterpret_cast<char*>( m_end ) - 
+                                        reinterpret_cast<char*>( m_begin );
 
-        // template
-        //     <
-        //         class u_arg = t_arg
-        //     >
-        // traits::enable_if
-        //     <
-        //         traits::is_nothrow_move_constructible<u_arg>::value ||
-        //        !traits::is_copy_constructible<u_arg>        ::value, 
-        //         void
-        //     > ::type
-        // push_back( const t_arg& element )
-        // {
+                const size_t new_size = old_size << grow_constant;
 
-        // }
+                t_arg *m_begin_new    = reinterpret_cast<t_arg*>( operator new ( new_size ) );
+
+                m_end                 = reinterpret_cast<t_arg*>( m_begin_new + old_size );
+                m_cap_end             = reinterpret_cast<t_arg*>( m_begin_new + new_size );
+
+                for ( t_arg *m_begin_new_it = m_begin_new; m_begin_new_it != m_end; ++m_begin, ++m_begin_new_it )
+                    new ( m_begin_new_it ) t_arg( *m_begin );
+
+                m_begin               = reinterpret_cast<t_arg*>( m_begin_new );                    
+            }
+
+            new ( m_end++ ) t_arg( element );
+        }
 
     private:
+
         t_arg   *m_begin;
         t_arg   *m_end;
         t_arg   *m_cap_end;
